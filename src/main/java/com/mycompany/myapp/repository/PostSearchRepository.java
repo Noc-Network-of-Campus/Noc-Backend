c089208c296aaf5046d38f559cd0758afa92b3f3
Jaeyun Park <qkrwodbsfjq@khu.ac.kr>
2025-05-07 05:47:56 +0900

    [Feat] 게시글 검색 API 구현 #4

---
+++ src/main/java/com/mycompany/myapp/repository/PostSearchRepository.java
@@ -0,0 +1,13 @@
+package com.mycompany.myapp.repository;
+
+import com.mycompany.myapp.domain.PostDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface PostSearchRepository extends ElasticsearchRepository<PostDocument, String> {
+    // 제목에 키워드가 포함된 게시글 검색
+    Page<PostDocument> findByTitleContaining(String keyword, Pageable pageable);
+}
